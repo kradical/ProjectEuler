7c1237b17686ca3705757e04a77a6ecb8dd458c4
Konrad <konrad_here@hotmail.com>
2015-03-11 19:36:33 -0700

    problem 13 in cs
    problem is set up not complete

---
+++ csproblems/problem13/problem13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("LELE GOTCHU GOOD");
+        }
+    }
+}
